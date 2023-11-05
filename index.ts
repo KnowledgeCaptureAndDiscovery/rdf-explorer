435142461aed4e38ce8471814f5e17612519069b
Maximiliano Osorio <maxiosorio@gmail.com>
2023-11-05 12:25:14 -0300

    fix: content negotation is working

--- index.ts
+++ index.ts
@@ -1,28 +1,49 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
-import config from "./config/production";
-
-//For env File
-dotenv.config();
-
+import performQuery from "./query";
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/*", (req: Request, res: Response) => {
+  //Get path, query, fragment from the request
+  const { path } = req;
+  const uri = `https:/${path}`;
   res.format({
-    "text/plain": function () {
-      res.send("Welcome to the home page!");
-    },
-
     "text/html": function () {
       res.send("<p>Welcome to the home page!</p>");
     },
-
-    "text/turtle": function () {
-      console.log("sending query " + config.endpoint.url);
-      res.send({ message: "Welcome to the home page!" });
+    "application/n-quads": async function () {
+      const stream = await performQuery(uri, "application/n-quads");
+      return stream.pipe(res);
     },
-
+    "application/n-triples": async function () {
+      const stream = await performQuery(uri, "application/n-triples");
+      return stream.pipe(res);
+    },
+    "application/rdf+xml": async function () {
+      const stream = await performQuery(uri, "application/rdf+xml");
+      return stream.pipe(res);
+    },
+    "application/trig": async function () {
+      const stream = await performQuery(uri, "application/trig");
+      return stream.pipe(res);
+    },
+    "application/trix": async function () {
+      const stream = await performQuery(uri, "application/trix");
+      return stream.pipe(res);
+    },
+    "application/turtle": async function () {
+      const stream = await performQuery(uri, "application/turtle");
+      return stream.pipe(res);
+    },
+    "application/x-turtle": async function () {
+      const stream = await performQuery(uri, "application/x-turtle");
+      return stream.pipe(res);
+    },
+    "application/ld+json": async function () {
+      const stream = await performQuery(uri, "application/ld+json");
+      return stream.pipe(res);
+    },
     default: function () {
       // log the request and respond with 406
       res.status(406).send("Not Acceptable");
--- query.ts
+++ query.ts
@@ -1,14 +1,35 @@
-import { Parser } from "sparqljs";
+import { Parser, Generator } from "sparqljs";
+import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
+import { QueryEngine } from "@comunica/query-sparql";
 
-const sendQuery = (uri: string, format) => {
-  const parser = new Parser();
-  const query = `CONSTRUCT { ?s ?p ?o }
-  WHERE  {
-    GRAPH < ${uri} >
-    { ?s ?p ?o }
-  } LIMIT 10 `;
-  parser.parse(query);
+import config from "./config/production";
 
-  console.log("sending query " + uri);
+const myEngine = new QueryEngine();
+
+const generator = new Generator();
+
+const performQuery = async (uri: string, format: string) => {
+  const query = createQuery(uri);
+  const triples = await sendQuery(query, format);
+  return triples;
 };
-export default sendQuery;
+
+const sendQuery = async (query: string, format: string) => {
+  const result = await myEngine.query(query, {
+    sources: [config.endpoint.endpointUrl],
+  });
+  const { data } = await myEngine.resultToString(result, format);
+  return data;
+};
+
+const createQuery = (uri: string): string => {
+  const parser = new Parser();
+  const query = `CONSTRUCT { <${uri}> ?p ?o }
+  WHERE  {
+    GRAPH ?g
+    { <${uri}> ?p ?o }
+  }`;
+  const parsedQuery = parser.parse(query);
+  return generator.stringify(parsedQuery);
+};
+export default performQuery;
